781794618c8bcf27b8d1caef4d20b1a71fcd964e
jorenvst <vansteenkiste.joren@telenet.be>
2025-05-22 23:30:32 +0200

    dismiss button

--- src/System/HNotif/Display/Window.hs
+++ src/System/HNotif/Display/Window.hs
@@ -5,6 +5,7 @@ import System.HNotif.Types
 
 import Graphics.UI.Gtk
 
+-- TODO: a function onDismiss should be passed, so when the dismiss button is pressed, the notification gets closed
 notificationWindow :: HNotifConfig -> Notification -> IO Window
 notificationWindow config n = do
     c <- notificationContainer config n
@@ -17,16 +18,20 @@ notificationWindow config n = do
     uncurry (windowSetDefaultSize w) (defaultSize . notificationView $ config)
     return w
 
-title :: HNotifConfig -> Notification -> IO Widget
-title config n = do
+summaryView :: HNotifConfig -> Notification -> IO Widget
+summaryView config n = do
     label <- labelNew (Nothing :: Maybe String)
     labelSetMarkup label ("<b>" ++ summary n ++ "</b>")
     align <- makeAlignment (summaryAlignment . notificationView $ config)
     containerAdd align label
     return $ toWidget align
 
-content :: HNotifConfig -> Notification -> IO Widget
-content config n = do
+-- TODO: onDismiss
+dismissButton :: IO Button
+dismissButton = buttonNewWithLabel "x"
+
+bodyView :: HNotifConfig -> Notification -> IO Widget
+bodyView config n = do
     label <- labelNew $ Just (body n)
     labelSetLineWrap label True
     align <- makeAlignment (bodyAlignment . notificationView $ config)
@@ -36,8 +41,11 @@ content config n = do
 notificationContainer :: HNotifConfig -> Notification -> IO Widget
 notificationContainer config n = do
     vbox <- vBoxNew False (padding . notificationView $ config)
-    title config n >>= \w -> boxPackStart vbox w PackGrow 0
-    content config n >>= \w -> boxPackStart vbox w PackGrow 0
+    hbox <- hBoxNew False (padding . notificationView $ config)
+    summaryView config n >>= \w -> boxPackStart hbox w PackGrow 0
+    dismissButton >>= \w -> boxPackStart hbox w PackNatural 0
+    boxPackStart vbox hbox PackGrow 0
+    bodyView config n >>= \w -> boxPackStart vbox w PackGrow 0
     return $ toWidget vbox
 
 makeAlignment :: TextAlignment -> IO Alignment
